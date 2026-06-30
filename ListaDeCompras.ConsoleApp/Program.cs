29d5567758ad229e34f34fa2a67b4d7f1095f5f0
Lucas de Lima <lucasdlcvieira@gmail.com>
2026-06-30 11:49:32 -0300

    Lista de compras

---
+++ ListaDeCompras.ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using ListaDeCompras.ConsoleApp.Compartilhado;
+
+TelaPrincipal telaPrincipal = new TelaPrincipal();
+
+while (true)
+{
+    ITelaOpcoes? telaSelecionada = telaPrincipal.ObterOpcaoMenuPrincipal();
+
+    if (telaSelecionada == null)
+        break;
+
+    while (true)
+    {
+        string? opcaoMenuInterno = telaSelecionada.ObterOpcaoMenu();
+
+        if (opcaoMenuInterno == "S")
+            break;
+
+        if (telaSelecionada is TelaBase telaBase)
+        {
+            if (opcaoMenuInterno == "1")
+                telaBase.Cadastrar();
+
+            else if (opcaoMenuInterno == "2")
+                telaBase.Editar();
+
+            else if (opcaoMenuInterno == "3")
+                telaBase.Excluir();
+
+            else if (opcaoMenuInterno == "4")
+                telaBase.VisualizarTodos(true);
+        }
+    }
+}
